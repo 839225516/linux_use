093ddbd4d0299b65e2a445abeb5c9219c0067681
839225516 <34149152+839225516@users.noreply.github.com>
2018-05-31 09:37:18 +0800

    Update tomcat_restart.sh

--- shell/tomcat_restart.sh
+++ shell/tomcat_restart.sh
@@ -1,15 +1,13 @@
 #!/bin/bash 
-
-set -eu
-
-#修改tomcat的路径
-PROG="Djava.util.logging.config.file=/data/java_service/tomcat/conf/logging.properties"
-PID=`ps -ef |grep "$PROG"|grep -v grep |awk  '{print $2}'`
-echo "tomcat的pid: $PID"
+DIR=$(dirname $( cd "$( dirname "$0" )" && pwd ))
+echo "服务路径：" $DIR
+SERVICE_NAME="Djava.util.logging.config.file=${DIR}/conf/logging.properties"
+PID=`ps -ef |grep "$SERVICE_NAME"|grep -v grep |awk  '{print $2}'`
+#echo $PID
 if [ "$PID" == "" ];then
-    echo -e "\e[1;31mprogress is not running\e[0m!\n\e[1;34mStarting progress\e[0m ..."
-    sh startup.sh
-
+    echo -e "\e[1;31mprogress is not running\e[0m\n\e[1;34mStarting progress\e[0m ..."
+    sh $DIR/bin/startup.sh
+    
 else
     echo -n "Stopping progress ..."
     kill $PID
@@ -21,5 +19,5 @@ else
     done
 
     echo -e "\n\e[1;34mRestart progress\e[0m ... "
-    sh startup.sh
+    sh $DIR/bin/startup.sh
 fi
