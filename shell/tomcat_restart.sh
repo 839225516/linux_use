2c6836847982c605c5c91291fd376a41a021a0ab
839225516 <34149152+839225516@users.noreply.github.com>
2018-10-25 10:09:11 +0800

    Update tomcat_restart.sh

--- shell/tomcat_restart.sh
+++ shell/tomcat_restart.sh
@@ -5,11 +5,11 @@ SERVICE_NAME="Djava.util.logging.config.file=${DIR}/conf/logging.properties"
 PID=`ps -ef |grep "$SERVICE_NAME"|grep -v grep |awk  '{print $2}'`
 #echo $PID
 if [ "$PID" == "" ];then
-    echo -e "\e[1;31mprogress is not running\e[0m\n\e[1;34mStarting progress\e[0m ..."
+    echo -e "\e[1;31m$(date +%Y/%m/%d-%H:%M:%S)    progress is not running\e[0m\n\e[1;34mStarting progress\e[0m ..."
     sh $DIR/bin/startup.sh
     
 else
-    echo -n "Stopping progress ..."
+    echo -n "$(date +%Y/%m/%d-%H:%M:%S)    Stopping progress ..."
     kill $PID
 
     while kill -0 $PID
@@ -18,6 +18,6 @@ else
         sleep 2
     done
 
-    echo -e "\n\e[1;34mRestart progress\e[0m ... "
+    echo -e "\n\e[1;34m$(date +%Y/%m/%d-%H:%M:%S)    Restart progress\e[0m ... "
     sh $DIR/bin/startup.sh
 fi
