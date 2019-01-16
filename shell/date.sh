8f0c45cbf1674a7cd06dc5420fc206de077fcd07
839225516 <34149152+839225516@users.noreply.github.com>
2019-01-16 09:13:39 +0800

    Update date.sh

--- shell/date.sh
+++ shell/date.sh
@@ -4,9 +4,9 @@
 CUR_TIME=`date +%s`
 echo "当前时间戳：$CUR_TIME "
 
-CUR_DAY=`date +%f -d @$CUR_TIME`
+CUR_DAY=`date +%F -d @$CUR_TIME`
 echo "当前的日期：$CUR_DAY"
 
-YESTERDAY=`date +%f -d @$(( CUR_TIME - 86400))`
-echo "昨天的日期： $YESTERDAY"
+YESTERDAY=`date +%F -d @$(( CUR_TIME - 86400))`
+echo "昨天的日期：$YESTERDAY"
 
